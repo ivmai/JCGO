3cd97e9d362a2d52418035b81e705c3c8533aa20
Ivan Maidanski <ivmai@mail.ru>
2012-08-30 21:42:15 +0400

    C runtime: Fix jcgo_JniFindClass - do not perform class initialization

--- include/jcgojniv.c
+++ include/jcgojniv.c
@@ -3,7 +3,7 @@
  * a part of the JCGO runtime subsystem.
  **
  * Project: JCGO (http://www.ivmaisoft.com/jcgo/)
- * Copyright (C) 2001-2010 Ivan Maidanski <ivmai@ivmaisoft.com>
+ * Copyright (C) 2001-2012 Ivan Maidanski <ivmai@ivmaisoft.com>
  * All rights reserved.
  */
 
@@ -397,13 +397,6 @@ jcgo_JniFindClass( JNIEnv *pJniEnv, CONST char *name )
   }
    else
    {
-#ifdef JCGO_STDCLINIT
-    jcgo_clinitTrig(baseClass);
-#else
-#ifdef JCGO_CLINITCHK
-    jcgo_clinitCheckOrder(baseClass);
-#endif
-#endif
     aclass = baseClass;
    }
  }
