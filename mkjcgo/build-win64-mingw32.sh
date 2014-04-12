9177a50043fa35d206e206a1a06e5efbc3981ddd
Ivan Maidanski <ivmai@mail.ru>
2014-04-12 09:14:55 +0400

    Add build-win64-mingw32.sh (rebuild BDWGC for Win64 by mingw-w64)

---
+++ mkjcgo/build-win64-mingw32.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+set -ex
+
+# Build JCGO Win64 binaries for mingw-w64 (mingw32).
+#
+# Prerequisites:
+# * mingw-w64 x86_64-w64-mingw32-gcc.exe (GCC) 4.5.2+
+# * wget http://www.hboehm.info/gc/gc_source/gc-7.4.0.tar.gz | tar xf -C contrib - ; mv gc-7.4.0 bdwgc
+# * wget http://www.hboehm.info/gc/gc_source/libatomic_ops-7.4.0.tar.gz | tar xf -C contrib - ; mv libatomic_ops-7.4.0 bdwgc/libatomic_ops
+
+AR=x86_64-w64-mingw32-ar
+CC="x86_64-w64-mingw32-gcc -m64"
+ARCH=amd64
+SYST=mingw64
+
+# Set current working directory to JCGO root:
+cd $(dirname "$0")/..
+
+# Build BDWGC (static single- and multi-threaded):
+mkdir -p libs/$ARCH/$SYST
+mkdir -p .build_tmp/libs-gc-$ARCH-$SYST .build_tmp/libs-gcmt-$ARCH-$SYST
+(cd .build_tmp/libs-gc-$ARCH-$SYST; $CC -O2 -fno-strict-aliasing \
+    -Wall -Wextra -DALL_INTERIOR_POINTERS -DJAVA_FINALIZATION \
+    -DGC_GCJ_SUPPORT -DNO_DEBUGGING -DDONT_USE_USER32_DLL -DUNICODE \
+    -I ../../contrib/bdwgc/include -c ../../contrib/bdwgc/*.c \
+    ../../contrib/bdwgc/*.cpp; $AR crus ../../libs/$ARCH/$SYST/libgc.a *.o)
+(cd .build_tmp/libs-gcmt-$ARCH-$SYST; $CC -O2 -fno-strict-aliasing \
+    -Wall -Wextra -DALL_INTERIOR_POINTERS -DJAVA_FINALIZATION \
+    -DGC_GCJ_SUPPORT -DNO_DEBUGGING -DLARGE_CONFIG -DUSE_MUNMAP -DGC_THREADS \
+    -DTHREAD_LOCAL_ALLOC -DPARALLEL_MARK -DDONT_USE_USER32_DLL -DUNICODE \
+    -I ../../contrib/bdwgc/include -I ../../contrib/bdwgc/libatomic_ops/src \
+    -c ../../contrib/bdwgc/*.c ../../contrib/bdwgc/*.cpp; \
+    $AR crus ../../libs/$ARCH/$SYST/libgcmt.a *.o)
+
+# Test compile jcgon:
+mkdir -p .build_tmp/test-jcgon-$ARCH-$SYST
+(cd .build_tmp/test-jcgon-$ARCH-$SYST; $CC -O2 -fwrapv \
+    -fno-strict-aliasing -Wall -DJCGO_FFDATA -DJCGO_LARGEFILE -DJCGO_EXEC \
+    -DJCGO_WIN32 -DJCGO_INET -DJCGO_ERRTOLOG -DJCGO_SYSWCHAR \
+    -I ../../include -c ../../native/*.c)
