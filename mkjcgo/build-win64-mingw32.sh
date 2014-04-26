34b72ea04e1b36950ffd858e1c5ef9dbb2c43e79
Ivan Maidanski <ivmai@mail.ru>
2014-04-26 20:56:54 +0400

    build-win64-mingw32.sh: Add build of trjnic dynamic library

--- mkjcgo/build-win64-mingw32.sh
+++ mkjcgo/build-win64-mingw32.sh
@@ -7,10 +7,13 @@ set -ex
 # * mingw-w64 x86_64-w64-mingw32-gcc.exe (GCC) 4.5.2+
 # * (cd contrib; curl http://www.hboehm.info/gc/gc_source/gc-7.4.0.tar.gz | tar zxf -; mv gc-7.4.0 bdwgc)
 # * (cd contrib/bdwgc; curl http://www.hboehm.info/gc/gc_source/libatomic_ops-7.4.0.tar.gz | tar zxf -; mv libatomic_ops-7.4.0 libatomic_ops)
+# * Oracle JDK 1.6.0+ (Windows x64)
+# * set JAVA_HOME=<path_to_jdk>
 
 AR=x86_64-w64-mingw32-ar
 CC="x86_64-w64-mingw32-gcc -m64"
 ARCH=amd64
+BASESYS=win32
 SYST=mingw64
 
 # Set current working directory to JCGO root:
@@ -38,3 +41,9 @@ mkdir -p .build_tmp/test-jcgon-$ARCH-$SYST
     -fno-strict-aliasing -Wall -DJCGO_FFDATA -DJCGO_LARGEFILE -DJCGO_EXEC \
     -DJCGO_WIN32 -DJCGO_INET -DJCGO_ERRTOLOG -DJCGO_SYSWCHAR \
     -I ../../include -c ../../native/*.c)
+
+# Build "trjnic" dynamic library (to enable TraceJni utility on Windows):
+mkdir -p auxbin/$ARCH/$BASESYS
+$CC -O2 -Wall -Wextra -DJCGO_WIN32 \
+    -I $JAVA_HOME/include/$BASESYS -I $JAVA_HOME/include \
+    -shared -o auxbin/$ARCH/$BASESYS/trjnic.dll -s reflgen/trjnic.c
